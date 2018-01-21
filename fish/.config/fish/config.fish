dace529219e981c9f67e9a82c82fa323ed41be13
Jose Benito Gonzalez Lopez <jose.benito.gonzalez@cern.ch>
2018-01-21 14:29:52 +0100

    cleaning fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,10 @@
+export SRC_HOME=<write_path_to_src>
+
+# Force local path (e.g. python3 from brew)
+
 set -gx PATH /usr/local/bin /usr/local/sbin $PATH
+
+# Virtualfish
 
 eval (python3 -m virtualfish compat_aliases)
 
@@ -6,22 +12,35 @@ if set -q VIRTUAL_ENV
     echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 end
 
+# virtualenvwrapper
+
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$SRC_HOME
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# source /usr/local/bin/virtualenvwrapper.sh
+
+# Some aliases
 
 alias grep='grep -i --colour'
 alias ll='ls -la'
 alias listening='lsof -Pni'
 
-export EDITOR='vim'
-# export LC_CTYPE=en_US.UTF-8
+# Exporting
 
-# virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/cernbox/CERN/dev/src
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export SQLALCHEMY_DATABASE_URI_CLAIMSTORE=postgresql://user:pass@localhost/claimstore
-export SQLALCHEMY_DATABASE_URI_INVENIO=postgresql://user:pass@localhost/invenio
+#export LC_CTYPE=en_US.UTF-8
+export EDITOR='vim'
+export SQLALCHEMY_DATABASE_URI_CLAIMSTORE=postgresql://user:password@localhost/claimstore
+export SQLALCHEMY_DATABASE_URI_INVENIO=postgresql://user:password@localhost/invenio
 export SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI_INVENIO
-# source /usr/local/bin/virtualenvwrapper.sh
+
+# Initiate in a src directory
+cd $SRC_HOME 
+
+# openstack and docker
+
+#function rssh() { ssh -C -i cloud.key root@$RDOCKERHOST "$@" ;}
+#function rdocker() { ssh -C -i cloud.key root@$RDOCKERHOST "docker $@" ;}
+#export RDOCKERHOST=some.remote.host.com
 
 # docker
 
@@ -31,17 +50,7 @@ export SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI_INVENIO
 #}
 
 # encrypt
+
 #function enc() { openssl enc -des3 -salt -in $1 -out $1.enc ;}
 #function dec() { openssl enc -d -des3 -in $1.enc -out $1 ;}
 #function dbuild-enc() { dec Dockerfile-production; docker build -f Dockerfile-production .; enc Dockerfile-production; rm Dockerfile-production; }
-
-cd $HOME/cernbox/CERN/dev/src
-
-# tmux
-# ssh() {
-#  tmux rename-window "$*"
-#  command ssh "$@"
-#  echo "Counting to 60"
-#  sleep 60 && exit
-#  tmux rename-window "bash (exited ssh)"
-#}
